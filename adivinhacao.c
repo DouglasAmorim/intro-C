b2c2fc86dc48f86ba0f60075792ed28e22d25179
Douglas Amorim <amorimdos@gmail.com>
2021-04-28 23:07:48 -0300

    start introdução ao C

---
+++ adivinhacao.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() { 
+	//olá
+	printf("***********************\n");
+	printf("bem vindo a essa budega\n");
+	printf("***********************\n");
+
+	int numeroSecreto = 42;
+
+	printf("O numero é %d: \n", numeroSecreto );
+}
