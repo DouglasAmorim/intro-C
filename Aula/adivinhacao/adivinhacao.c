530b620a3d540905d342a234b12bdf68e4e17f4f
Douglas Amorim <amorimdos@gmail.com>
2021-05-01 11:48:54 -0300

    finished C I classes

--- Aula/adivinhacao/adivinhacao.c
+++ Aula/adivinhacao/adivinhacao.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define NUMERO_DE_TENTATIVAS 5
 
@@ -7,13 +9,41 @@ int main() {
 	printf("bem vindo a essa budega\n");
 	printf("***********************\n\n");
 
-	int numeroSecreto = 42;
+	int segundos = time(0);
+	srand(segundos);
+	int numeroAleatorio =  rand();
+
+	int numeroSecreto = numeroAleatorio % 100;
+	// Só usar depois de iniciarlizar
 	int chute;
+	
 	int ganhou = 0;
 	int tentativa = 1;
 
-	// for(int i = 0; i  < NUMERO_DE_TENTATIVAS; i++) {
-	while(!ganhou) {
+	float pontos = 1000.0;
+
+	int nivel = 0;
+	printf("Qual o nível de dificuldade? \n");
+	printf("(0) Fácil (1) Médio (2) Dificil\n\n");
+	printf("Escolha: \n");
+	scanf("%d", &nivel);
+
+	int numeroTentativas = 0;
+
+	switch(nivel) {
+		case 0:
+			numeroTentativas = 20;
+			break;
+		case 1:
+			numeroTentativas = 10;
+			break;
+		default: 
+			numeroTentativas = 5;
+			break;
+	}
+
+	for(int i = 0; i  < numeroTentativas; i++) {
+	// while(!ganhou) {
 		printf("Tentativa %d \n", tentativa);
 
 		printf("Qual seu palpite? \n");
@@ -30,19 +60,32 @@ int main() {
 		int menor = chute < numeroSecreto;
 
 		printf("Seu chute foi %d\n", chute);
+		float pontosPerdidos;
 
 		if (ganhou) {
-			printf("Parabens voce acertou em %d tentativas! \n\n",tentativa);
+			break;
 		} else if (maior) {
 			printf("Infelizmente voce errou \n");
 			printf("Seu chute foi maior que o numero secreto \n\n");
+			
 		} else { 
 			printf("Infelizmente voce errou \n");
 			printf("Seu chute foi menor que o numero secreto \n\n");
 		}
 
+		pontosPerdidos = abs((double)(chute - numeroSecreto) / (double)2);
+
+		pontos = pontos - pontosPerdidos;
 		tentativa++;
 		
+	}
+
+	if (ganhou) {
+		printf("Voce Ganhou \n");
+		printf("Parabens voce acertou em %d tentativas! \n\n",tentativa);
+		printf("Você fez %.1f pontos\n", pontos);
+	} else {
+		printf("Voce perdeu \n");
 	}
 
 	printf("Fim de Jogo\n");
---
+++ Aula/adivinhacao/casting.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main() { 
+	int a = 3;
+	int b = 2;
+
+	// CAST
+	double pontos = a / (double)b;
+	printf("%.2f\n", pontos );
+}
